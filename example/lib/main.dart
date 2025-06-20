04fae8e2109904131c52cb756da459e583c3a44e
Vignaraj <vignaraj@Vignaraj-MacBook-Pro.local>
2025-06-20 14:39:37 +0530

    chore: rename package to theme_maker and update all references

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:theme_kit/theme_kit.dart';
+import 'package:theme_maker/theme_maker.dart';
 
 void main() {
   runApp(const MyApp());
--- test/theme_maker_test.dart
+++ test/theme_maker_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:theme_kit/theme_kit.dart';
+import 'package:theme_maker/theme_maker.dart';
 import 'package:flutter/material.dart';
 
 void main() {
